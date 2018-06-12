a06acda15a3d71b42b293c05a93a75eb8cff6682
qshuai <qishuai231@gmail.com>
2018-06-12 13:46:33 +0800

    Add install docker script

---
+++ docker-install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+sudo add-apt-repository \
+"deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
