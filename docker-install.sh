8fec3680ed1b3145ef35a0ade16c0d9ded6b8000
qshuai <qishuai231@gmail.com>
2018-06-13 20:24:06 +0800

    Rewrite docker installation script

--- docker-install.sh
+++ docker-install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-curl -fsSL get.docker.com -o get-docker.sh
-sudo sh get-docker.sh --mirror Aliyun
-rm get-docker.sh
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+sudo add-apt-repository "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
